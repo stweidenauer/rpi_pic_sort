74146ced16cd93bded59b90a63e599237143879b
Steffen <stweidenauer@gmail.com>
2020-10-12 17:37:51 +0200

    # ficed directories and tested on ubuntu

--- sort_web_pics.py
+++ sort_web_pics.py
@@ -11,7 +11,7 @@ def find_today():
 
 
 # returns ten days prior string formatted: YYYY-MM-DD
-def find_last_ten_days():
+def find_last_five_days():
     day_list = []
     today = datetime.now()
     for i in range(10):
@@ -20,29 +20,35 @@ def find_last_ten_days():
 
 
 def ten_days_prior():
-    return (datetime.now()-timedelta(days=10)).strftime("%Y-%m-%d")
+    return (datetime.now() - timedelta(days=9)).strftime("%Y-%m-%d")
 
 
 def start():
     # define base_dir and target directory
-    base_dir = os.path.join('/', 'home', 'pi', 'webcam')
-    target_dir = os.path.join('/', 'home', 'pi', 'flask_app', 'app', 'static')
+    base_dir = os.path.join('/', 'home', 'steffen', 'GitHub', 'rpi_pic_sort')
+    target_dir = os.path.join('/', 'home', 'steffen', 'GitHub', 'rpi_pic_sort', 'test')
 
     # create target folder
-    os.chdir(target_dir)
-    for item in find_last_ten_days():
+    list_last_five_days = find_last_five_days()
+    for item in list_last_five_days:
+        os.chdir(target_dir)
         if not os.path.isdir('./' + item):
             os.mkdir(item)
+            print('Directory ' + item + 'created')
+        new_target_dir = os.path.join(target_dir, item)
         os.chdir(base_dir)
         for picture in os.listdir():
             if picture.startswith(item):
-                shutil.move(picture, os.path.join(target_dir, item))
+                shutil.move(picture, new_target_dir)
 
     # removes folders that is 10 days old
+    os.chdir(target_dir)
+    ten_days = ten_days_prior()
     for folder in os.listdir():
-        if folder.startswith(ten_days_prior()) and os.path.isdir(folder):
+        if folder.startswith('2020-10-03') and os.path.isdir(folder):
             shutil.rmtree(folder)
-
+            print('Folder gefunden und gelösch')
+            
 
 if __name__ == "__main__":
     start()
