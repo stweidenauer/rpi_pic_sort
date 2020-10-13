627d8eb1f4f59ac5a698a962254e715fd2289425
Steffen <stweidenauer@gmail.com>
2020-10-13 15:23:58 +0200

    # all fixed not very elegant though

--- sort_web_pics.py
+++ sort_web_pics.py
@@ -11,7 +11,7 @@ def find_today():
 
 
 # returns ten days prior string formatted: YYYY-MM-DD
-def find_last_five_days():
+def find_last_ten_days():
     day_list = []
     today = datetime.now()
     for i in range(10):
@@ -29,26 +29,30 @@ def start():
     target_dir = os.path.join('/', 'home', 'pi', 'flask_app', 'app', 'static')
 
     # create target folders
-    list_last_five_days = find_last_five_days()
-    for item in list_last_five_days:
+    list_last_ten_days = find_last_ten_days()
+    for item in list_last_ten_days:
         os.chdir(target_dir)
         if not os.path.isdir('./' + item):
             os.mkdir(item)
             print('Directory ' + item + 'created')
+
+    # move pictures to new folder
+    os.chdir(base_dir)
+    print(base_dir)
+    for item in list_last_ten_days:
         new_target_dir = os.path.join(target_dir, item)
-        os.chdir(base_dir)
-        # move pictures to new folder
         for picture in os.listdir():
             if picture.startswith(item):
                 shutil.move(picture, new_target_dir)
+                print('picture ' + picture + 'moved')
 
     # removes folders that is 10 days old
     os.chdir(target_dir)
     ten_days = ten_days_prior()
     for folder in os.listdir():
-        if folder.startswith('2020-10-03') and os.path.isdir(folder):
+        if folder.startswith(ten_days) and os.path.isdir(folder):
             shutil.rmtree(folder)
-            print('Folder gefunden und gelösch')
+            print('Folder gefunden und gelöscht')
 
 
 if __name__ == "__main__":
